74f60495620e2803657e8f0ba2a924cf179e60a9
Łukasz Nowicki <lukasz.nowicki@xfive.co>
2018-02-25 18:11:24 +0100

    Tworzenie i logowanie użytkownika

--- wordpress/wp-content/plugins/KSUser/KS/Plugin/User/Plugin.php
+++ wordpress/wp-content/plugins/KSUser/KS/Plugin/User/Plugin.php
@@ -64,6 +64,17 @@ class Plugin {
 	}
 
 	/**
+	 * @param $user_id
+	 */
+	function login_user_by_id( $user_id ) {
+		wp_clear_auth_cookie();
+		wp_set_current_user ( $user_id );
+		wp_set_auth_cookie  ( $user_id );
+		wp_safe_redirect( home_url() );
+		exit();
+	}
+
+	/**
 	 * Autoryzacja właściwa
 	 */
 	function require_authorization() {
@@ -78,9 +89,33 @@ class Plugin {
 		} elseif ( 200 !== $error ) {
 			$message = 'Wystąpił nieznany błąd. Konieczna jest ' . $this->auth_link('ponowna autoryzacja') . ' w systemie Księstwa Sarmacji';
 		}
+		if ( is_null( $this->ks_user ) || empty( $this->ks_user ) || !isset( $this->ks_user ) ) {
+			$message = 'Aby przeglądać tę stronę musisz ' . $this->auth_link('autoryzować aplikację') . '.';
+		}
 		if ( '' != $message ) {
 			$this->show_error( $message );
 		}
+		$user_query = new \WP_User_Query( array( 'meta_key' => 'KSI_paszport', 'meta_value' => $user['paszport'] ,  'fields' => 'all'  ) );
+		$user_table = $user_query->get_results();
+		if ( is_array( $user_table ) && ( 1 === count( $user_table ) ) ) {
+			$wp_user = $user_table[0];
+			$this->login_user_by_id( $wp_user->ID );
+		}
+		if ( isset( $this->ks_user['paszport'] ) && ( '' !== $this->ks_user['paszport'] ) ) {
+			$new_user_password = wp_generate_password(32,TRUE);
+			$new_user_email = ( isset( $this->ks_user['email'] ) && ( '' != $this->ks_user['email'] ) ) ? $this->ks_user['email'] : $this->ks_user['paszport'] . '@sarmacja.org';
+			$new_user_id = wp_create_user( $this->ks_user['paszport'], $new_user_password, $new_user_email );
+			wp_update_user([
+				'ID' => $new_user_id,
+				'nickname' => $this->ks_user['nick'] . ' [' . $this->ks_user['paszport'] . ']'
+			]);
+			add_user_meta( $new_user_id, 'KSI_paszport', $this->ks_user['paszport'], TRUE );
+			add_user_meta( $new_user_id, 'KSI_data', $this->ks_user, TRUE );
+			$ready_user = new \WP_User( $new_user_id );
+			$ready_user->set_role('subscriber');
+			$this->login_user_by_id( $new_user_id );
+		}
+		exit('Coś poszło bardzo nie tak.');
 	}
 
 }
